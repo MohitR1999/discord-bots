19ae4cd26f8928adec01a101ae7504a819e387b1
MohitR1999 <mohitr1999@gmail.com>
2026-02-09 23:34:17 +0530

    removed all stuff to start over with different approach

--- packages/utils/src/main.ts
+++
@@ -1,4 +0,0 @@
-export { Bot } from './types/Bot'
-export type { BotProps } from './types/Bot'
-export type { Command } from './types/Command'
-export { Controller } from './types/Controller'
--- packages/utils/src/types/Command.ts
+++
@@ -1,6 +0,0 @@
-interface Command<CommandType, EventType> {
-  data: CommandType;
-  execute(param: EventType): Promise<void>;
-}
-
-export type { Command }
