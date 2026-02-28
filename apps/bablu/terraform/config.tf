8a45a2d15cf7aabf61ce0886b15a070be07c3209
MohitR1999 <mohitr1999@gmail.com>
2026-02-28 11:17:58 +0530

    added s3 directory for state

--- apps/bablu/terraform/config.tf
+++ apps/bablu/terraform/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "bablu-terraform-state"
-    key          = "terraform.tfstate"
+    key          = "terraform-state/"
     region       = "ap-south-1"
     use_lockfile = true
   }
