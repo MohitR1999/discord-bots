0de8698c8951d2245544cdff91b0ad951f5ec262
MohitR1999 <mohitr1999@gmail.com>
2026-02-28 18:23:31 +0530

    added ecs task role block

--- apps/bablu/terraform/iam.tf
+++ apps/bablu/terraform/iam.tf
@@ -44,6 +44,13 @@ resource "aws_iam_role" "ecs_execution_role" {
   tags = local.common_tags
 }
 
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "${var.project_name}-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume.json
+
+  tags = local.common_tags
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_execution_attach" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
