661e217220bf72e8f6eb4f3c36393cdb46fa2639
MohitR1999 <mohitr1999@gmail.com>
2026-03-01 14:38:31 +0530

    added CLIENT_ID as secret

--- apps/bablu/terraform/secrets.tf
+++ apps/bablu/terraform/secrets.tf
@@ -2,3 +2,8 @@ resource "aws_secretsmanager_secret" "discord_token" {
   name = "${var.project_name}-discord-token"
   tags = local.common_tags
 }
+
+resource "aws_secretsmanager_secret" "app_id" {
+  name = "${var.project_name}-app-id"
+  tags = local.common_tags
+}
